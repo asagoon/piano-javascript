2d91067e915b29690af4287a98d7c68987ab09d8
Yuta Asai <asagon.ngzk46@gmail.com>
2023-06-09 10:54:31 -0700

    changed javascript

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ let audio = new Audio("tunes/a.wav"); //by default, audio src is "a" tune
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`); //getting clicked key element
+    clickedKey.classList.add("active");
 }
 
 pianoKeys.forEach(key => {
     key.addEventListener("click", ()=> playTune(key.dataset.key));
 });
+
+const pressedKey = (e) => {
+    playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
