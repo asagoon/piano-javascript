8caba518fc95ec6a14aa092a4285714ef13175e8
Yuta Asai <asagon.ngzk46@gmail.com>
2023-06-09 12:02:31 -0700

    done adding details

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume input"),
+keysHide = document.querySelector(".checkBox input");
 
-let audio = new Audio("tunes/a.wav"); //by default, audio src is "a" tune
+let allKeys = [],
+audio = new Audio("tunes/a.wav"); //by default, audio src is "a" tune
 
 
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;
     audio.play();
+    //console.log(allKeys);
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); //getting clicked key element
-    clickedKey.classList.add("active");
+    clickedKey.classList.add("active"); //adding active class to the clicked key element
+    setTimeout(() => { //removing active class after 150 ms from the clicked key element 
+        clickedKey.classList.remove("active")
+    }, 150);
 }
 
 pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key); //adding data-key value to the allKeys array
+    //calling playTune function with passing data-key value as an argument
     key.addEventListener("click", ()=> playTune(key.dataset.key));
 });
 
-const pressedKey = (e) => {
-    playTune(e.key);
+const hideKeys = () => {
+    // toggling hide class from each key on the checkbox click
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
 }
 
+const handleVolume = (e) => {
+    audio.volume = e.target.value; //passing the range slider value as an audio volume
+}
+
+const pressedKey = (e) => {
+    //if the pressed key is in the allKeys array, only call the playTune function
+    if(allKeys.includes(e.key)) playTune(e.key);
+    console.log(e);
+}
+
+keysHide.addEventListener("click", hideKeys);
+volumeSlider.addEventListener("input", handleVolume);
 document.addEventListener("keydown", pressedKey);
